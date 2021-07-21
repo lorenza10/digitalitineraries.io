fa1fa20e7b6c9f68c3dacb333e43615d547ba4d6
lorenza1010 <lozzenge10@gmail.com>
2021-07-21 22:55:36 +0100

    Update

---
+++ README.md
@@ -0,0 +1,1 @@
+# digitalitineraries.io
---
+++ js/main.js
@@ -0,0 +1,16 @@
+function getData(url) {
+
+    var map = L.map('mapid', {
+            zoomControl: false
+        })
+        .setView([51.467376142760884, -3.1661224365234375, ], 6);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        accessToken: 'accessToken'
+    }).addTo(map);
+
+    var monarchGeoJSON = false;
+
+};
