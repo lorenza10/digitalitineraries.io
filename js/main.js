8981a85c624fb5e2a067b2458eaaca1aef4bd1c4
lorenza1010 <lozzenge10@gmail.com>
2021-07-22 15:11:20 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-function getData(url) {
+// function getData(url) {
+$(document).ready(function() {
 
+
+    $('#mapid').height(window.innerHeight);
     var map = L.map('mapid', {
             zoomControl: false
         })
@@ -12,5 +15,59 @@ function getData(url) {
     }).addTo(map);
 
     var monarchGeoJSON = false;
-
-};
+    url = 'data/json/countGeoffrey.json'
+    fetch(url, {
+            method: 'GET'
+        })
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
+            var min = 0;
+            var max = 0;
+            monarchGeoJSON = L.geoJSON(json, {
+                style: function(feature) {
+                    return {
+                        fillOpacity: 0.1,
+                        fillColor: '#000',
+                        color: '#000',
+                        opacity: 0.3
+                    };
+                },
+                pointToLayer: function(geoJsonPoint, latlng) {
+                    if (geoJsonPoint.properties.id < min || min === 0) {
+                        min = geoJsonPoint.properties.id;
+                    }
+                    if (geoJsonPoint.properties.id > max) {
+                        max = geoJsonPoint.properties.id;
+                    }
+                    var html = '';
+                    var arrayOfProps = ['title', 'id', 'place', 'time'];
+                    arrayOfProps.forEach(function(prop) {
+                        html += '<strong>' + prop + '</strong>' + ': ' + geoJsonPoint.properties[prop] + '<br/>'
+                    })
+                    return L.circle(latlng, 5000).bindPopup(html);
+                },
+            }).addTo(map);
+            var slider = document.getElementById('slider');
+            noUiSlider.create(slider, {
+                start: [min],
+                step: 1,
+                range: {
+                    'min': min,
+                    'max': max
+                }
+            }).on('slide', function(e) {
+                monarchGeoJSON.eachLayer(function(layer) {
+                    if (layer.feature.properties.id == parseFloat(e[0])) {
+                        $('#displayLocation').html(layer.feature.properties.location);
+                        $('#displayDate').html(layer.feature.properties.time);
+                        $('#displayInformation').html(layer.feature.properties.comments);
+                        layer.addTo(map);
+                    } else {
+                        map.removeLayer(layer);
+                    }
+                });
+            });
+        })
+        .catch(error => console.log(error.message));
+});
