fa112280a77cf6997056134887b4cdcf23c6a5fb
lorenza1010 <lozzenge10@gmail.com>
2021-07-24 16:50:55 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+function navBar() {
+    console.log('success')
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
 function getData(url) {
     if (map != undefined) {
         map.remove();
